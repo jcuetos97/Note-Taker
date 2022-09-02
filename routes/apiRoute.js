27bb2c398fba6975309706db46914c7928c8e00e
jcuetos97 <jcuetos97@gmail.com>
2022-09-02 11:50:41 -0500

    DELETE request method added

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,14 +1,17 @@
+// Dependencies
 const api = require('express').Router();
 const path = require ('path');
 const fs = require ('fs');
 var uniqid = require('uniqid');
 
 
+// Returns all saved notes as JSON 
 api.get('/notes', (req,res) => {
     console.log(`${req.method} request received.`);
     res.sendFile(path.join(__dirname, "../db/db.json"));
 });
 
+// Adds user note to database and returns new note to the client
 api.post('/notes', (req,res) => {
     console.log(`${req.method} request received.`);
     let database = JSON.parse(fs.readFileSync('./db/db.json', "utf8")); 
@@ -26,22 +29,15 @@ api.post('/notes', (req,res) => {
 
 });
 
+// Deletes selected note from database 
 api.delete('/notes/:id', (req,res) => {
- 
+    console.log(`${req.method} request received.`);
     let database = JSON.parse(fs.readFileSync('./db/db.json', "utf8")); 
+    const result = database.filter(note => note.id !== req.params.id);
+    const updatedNotes = fs.writeFileSync('./db/db.json', JSON.stringify (result));
 
-    if (req.params.id) {
-        const id = req.params.id;
-    }
-    
-    database.push(userNote);
-    fs.writeFileSync('./db/db.json', JSON.stringify(database))
-    console.log(database);
-    res.json(database);
-
+    res.json(result);
 });
-
-
 
 
 module.exports = api;
--- server.js
+++ server.js
@@ -1,28 +1,32 @@
+// Dependencies
 const express = require ('express');
 const path = require ('path');
 const api = require('./routes/apiRoute.js');
 
+// Port declared
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Express middleware functions
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+// 
 app.use('/api', api);
 
-
-
+// Sends notes.html file when request method GET /notes 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// Sends index.html file when request method GET *
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// App listener: starts server
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
